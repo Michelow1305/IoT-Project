8b5b96e2bd3c8a8955266f8f4269b353c1356301
Michel <micheljensen03@gmail.com>
2023-06-28 15:36:29 +0200

    whoopsie, little mistake

--- boot.py
+++ boot.py
@@ -1,6 +1,6 @@
 # boot.py -- run on boot-up
 import network, utime
-from machine import Pin
+from machine import Pin, reset
 
 onboard_LED = Pin("LED", Pin.OUT)
 onboard_LED.toggle()
@@ -16,11 +16,16 @@ def do_connect():
         #print('connecting to network...')
         sta_if.active(True)
         sta_if.connect(SSID, SSID_PASSWORD) # type: ignore
+        counter = 0
         while not sta_if.isconnected():
             #print("Attempting to connect....")
+            onboard_LED.toggle()
             utime.sleep_ms(100)
+            counter += 1
+            if(counter > 150):
+                reset()
     #print('Connected! Network config:', sta_if.ifconfig())
     
 #print("Connecting to your wifi...")
 do_connect()
-onboard_LED.toggle()
+onboard_LED.value(0)
