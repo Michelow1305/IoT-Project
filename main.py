88977e064880e4018183b3979999a42138160c78
Michel <micheljensen03@gmail.com>
2023-06-28 12:49:30 +0200

    Update for Pico D

--- main.py
+++ main.py
@@ -11,24 +11,22 @@ ADAFRUIT_USERNAME = "michelow"
 ADAFRUIT_PASSWORD = "aio_xnAU80fke1w7PYPBzSyxLnQZYAwO"
 SUBSCRIBE_TOPIC = b"michelow/f/gate"
 
-#print(f"Begin connection with MQTT Broker :: {MQTT_BROKER}")
-mqttClient = MQTTClient(CLIENT_ID, MQTT_BROKER, PORT, ADAFRUIT_USERNAME, ADAFRUIT_PASSWORD, keepalive=60)
-mqttClient.connect()
-#print(f"Connected to MQTT  Broker :: {MQTT_BROKER} successfully!")
-
-# pins
-led_onboard = machine.Pin("LED", machine.Pin.OUT)
-
 # MQTT subscribe callback (gate info)    
 def sub_cb(topic, msg):
-    if msg.decode() == "true":
-        led_onboard.value(1)
+    if msg.decode() == "True":
+        led.value(1)
     else:
-        led_onboard.value(0)
+        led.value(0)
 
+print(f"Begin connection with MQTT Broker :: {MQTT_BROKER}")
+mqttClient = MQTTClient(CLIENT_ID, MQTT_BROKER, PORT, ADAFRUIT_USERNAME, ADAFRUIT_PASSWORD, keepalive=60)
 mqttClient.set_callback(sub_cb) # whenever a new message comes (to picoW), print the topic and message (The call back function will run whenever a message is published on a topic that the PicoW is subscribed to.)
 mqttClient.connect()
 mqttClient.subscribe(SUBSCRIBE_TOPIC)
+print(f"Connected to MQTT  Broker :: {MQTT_BROKER} successfully!")
+
+# pins
+led = machine.Pin(20, machine.Pin.OUT)
 
 while True:
     mqttClient.check_msg()
