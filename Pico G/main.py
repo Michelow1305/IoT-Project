db9a7061362b5e74935247c448fb5896116c4f98
Michel <micheljensen03@gmail.com>
2023-06-30 13:49:54 +0200

    new "alive feed" included

--- Pico G/main.py
+++ Pico G/main.py
@@ -15,6 +15,12 @@ PORT = 1883
 ADAFRUIT_USERNAME = "michelow"
 ADAFRUIT_PASSWORD = "aio_xnAU80fke1w7PYPBzSyxLnQZYAwO"
 PUBLISH_TOPIC = b"michelow/f/gate"
+SUBSCRIBE_TOPIC = b"michelow/f/alive"
+
+picoDLastSeen = 0
+
+def sendAliveStatus():
+    mqttClient.publish(topic=PUBLISH_TOPIC, msg=str("G").encode(), qos=1) # type: ignore
 
 ## MQTT publish function with gate info    
 def publishGateState():
@@ -36,10 +42,18 @@ def interrupt_callback(pin):
         publishGateState()
         utime.sleep(1)
 
+def MQTT_subscribe_callback(topic, msg):
+    if msg.decode() == "D":
+        picoDLastSeen = utime.time()
+
+
 #print(f"Begin connection with MQTT Broker :: {MQTT_BROKER}")
 mqttClient = MQTTClient(CLIENT_ID, MQTT_BROKER, PORT, ADAFRUIT_USERNAME, ADAFRUIT_PASSWORD, keepalive=60)
 try:
+    mqttClient.set_callback(MQTT_subscribe_callback)
     mqttClient.connect()
+    utime.sleep(1)
+    mqttClient.subscribe(SUBSCRIBE_TOPIC, qos=1)
 except:
     machine.reset()
 #print(f"Connected to MQTT  Broker :: {MQTT_BROKER} successfully!")
@@ -51,6 +65,13 @@ publishGateState() # inital publish
 #print("Initial publish done, starting routine.")
 
 while True:
-    utime.sleep(100)
-    mqttClient.disconnect()
-    machine.reset()
+    try:
+        mqttClient.check_msg()
+
+        if(utime.time() - picoDLastSeen > 25):
+            machine.reset()
+
+        utime.sleep_ms(500)
+        sendAliveStatus()
+    except:
+        machine.reset()
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ubinascii import hexlify
 import machine
 from umqttsimple import MQTTClient
-from utime import sleep_ms, time
+from utime import sleep_ms, sleep, time
 
 ## pins
 led = machine.Pin(20, machine.Pin.OUT)
@@ -9,6 +9,7 @@ onboard_LED = machine.Pin("LED", machine.Pin.OUT)
 buzzer = machine.Pin(15, machine.Pin.OUT)
 currentGateStatus = True # True to prevent beeping while restart
 previousGateStatus = True 
+picoGLastSeen = 0
 
 ## MQTT Stuff
 CLIENT_ID = hexlify(machine.unique_id()) #To create an MQTT client, we need to get the PICOW unique ID
@@ -16,7 +17,8 @@ MQTT_BROKER = "io.adafruit.com" # MQTT broker IP address or DNS
 PORT = 1883
 ADAFRUIT_USERNAME = "michelow"
 ADAFRUIT_PASSWORD = "aio_xnAU80fke1w7PYPBzSyxLnQZYAwO"
-SUBSCRIBE_TOPIC = b"michelow/f/gate"
+SUBSCRIBE_TOPIC_1 = b"michelow/f/gate"
+SUBSCRIBE_TOPIC_2 = PUBLISH_TOPIC = b"michelow/f/alive"
 
 ## MQTT subscribe callback (gate info)    
 def MQTT_subscribe_callback(topic, msg):
@@ -26,9 +28,12 @@ def MQTT_subscribe_callback(topic, msg):
         if(led.value() is 0): # only beep when the gate just opened
             led.value(1)
             beep()
-    else:
+    elif msg.decode() == "0":
         currentGateStatus = False
         led.value(0)
+    elif msg.decode() == "G":
+        global picoGLastSeen
+        picoGLastSeen = time()
 
     # blink
     onboard_LED.toggle()
@@ -53,24 +58,30 @@ def beep():
     sleep_ms(200)
     buzzer.toggle()
 
+def sendAliveStatus():
+    mqttClient.publish(topic=PUBLISH_TOPIC, msg=str("D").encode(), qos=1) # type: ignore
+
 mqttClient = MQTTClient(CLIENT_ID, MQTT_BROKER, PORT, ADAFRUIT_USERNAME, ADAFRUIT_PASSWORD, keepalive=120)
 try:
     #print(f"Begin connection with MQTT Broker :: {MQTT_BROKER}")
     # whenever a new message comes (to picoW), execute callback function
     mqttClient.set_callback(MQTT_subscribe_callback)
     mqttClient.connect()
-    mqttClient.subscribe(SUBSCRIBE_TOPIC, qos=1)
+    sleep(1)
+    mqttClient.subscribe(SUBSCRIBE_TOPIC_1, qos=1)
+    mqttClient.subscribe(SUBSCRIBE_TOPIC_2, qos=1)
 except:
     machine.reset()
 #print(f"Connected to MQTT  Broker :: {MQTT_BROKER} successfully!")
 
-startTime_s = time()
-
 while True:
-    mqttClient.check_msg()
+    try:
+        mqttClient.check_msg()
 
-    timeElapsed_s = time() - startTime_s
-    if(timeElapsed_s > 120 and currentGateStatus is False):
+        if(time() - picoGLastSeen > 25):
+            machine.reset()
+
+        sleep_ms(500)
+        sendAliveStatus()
+    except:
         machine.reset()
-
-    sleep_ms(500)
